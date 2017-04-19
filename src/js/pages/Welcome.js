b4554ca6a9114e57f5db4385e94bf3fa20ebfe58
Deborah Oni <deborah.oni@andela.com>
2017-04-19 13:50:42 +0100

    feature:-input-to-search-through-sources-143900029

--- src/js/pages/Welcome.js
+++ src/js/pages/Welcome.js
@@ -3,8 +3,8 @@ import React from "react";
 import request from 'superagent';
 import SearchStore from '../stores/SearchStore';
 import {searchSources} from '../actions/SearchActions';
-
-
+const API_KEY="213327409d384371851777e7c7f78dfe";
+let link =  `https://newsapi.org/v1/articles?source={source.name}&sortBy=latest&apiKey={API_KEY}`
 export default class Welcome extends React.Component {
   
   constructor(){
@@ -12,11 +12,13 @@ export default class Welcome extends React.Component {
     this.getSources = this.getSources.bind(this);
     this.state ={
       sources: SearchStore.getAll(),
+      search: ""
     };
   }
   
   componentWillMount(){
-    // SearchStore.on(this.getSources);
+    
+        // SearchStore.on(this.getSources);
     searchSources();
   }
   
@@ -33,22 +35,35 @@ export default class Welcome extends React.Component {
       sources: SearchStore.getAll(),
     });
   }
+  
+  handleChange(event){
+    //const searchedSources = e.target.value;
+    //this.changeSources(this.state.sources);
+    //console.log(event);
+    this.setState({search: event.target.value})
+    console.log(this.state.search)
+  }
   render() {
-   const {sources} = this.state;
-    
+   const {sources} = this.state;   
+   const filteredSources = this.state.sources.filter(source=>source.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1); 
+  
     return(
     <div class="jumbotron">
       <h1>Headlines</h1>
       <p>News from over 70 sources
       </p>
-      <ul>
-      {
-        sources && sources.map(source=>{
-           return <div> <li>{source.name}{source.url}</li></div>
-        })
-      }
-      </ul>
-    </div>
+        <input class = "searchbox col col-lg-12" value={this.state.search} onChange={this.handleChange.bind(this)}/>
+      <div>
+    
+      {filteredSources.map(source=>{
+            return <div key={source.id}>
+                      <h4><a href= {link} >{source.name}</a></h4>
+                          <p>{source.description}</p>
+                      </div>                      
+
+        })}
+      </div>
+      </div>
     )
   }
 };
