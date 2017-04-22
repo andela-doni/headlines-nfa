af8fd1e0472f32ba6702650a5556b2ef3fdb9139
Deborah Oni <deborah.oni@andela.com>
2017-04-22 15:53:27 +0100

    express js works, redirecting issues still persists

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import path from 'path';
+var express = require('express');
+var path = require('path');
 
 // Create new app
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+app.use(express.static('src'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static('public'));
+
 
 app.listen(port, () => {
   console.log(`Express server is up on port ${port}`);
