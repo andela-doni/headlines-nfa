302814b8c346688084b989d3adddc2753d87085e
Deborah Oni <deborah.oni@andela.com>
2017-04-26 09:00:31 +0100

    sort types implemented

--- src/js/pages/Articles.js
+++ src/js/pages/Articles.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ArticlesStore from '../stores/ArticlesStore';
 import { getArticles } from '../actions/ArticlesActions';
 import { getSources } from '../actions/SourcesActions';
-import SourceStore from '../stores/SourceStore';
+import { getSorts } from '../actions/SortActions';
+import SortStore from '../stores/SortStore';
 
 
 export default class Articles extends React.Component {
@@ -10,19 +11,10 @@ export default class Articles extends React.Component {
     super();
     this.state = {
       articles: ArticlesStore.getAll(),
+      sortType:''
     };
 
     this.getArticles = this.getArticles.bind(this);
-
-
-    //   const mySources = this.state.sources;
-    //   mySources.map((mySource) => {
-    //     <option>{}</option>
-    //     console.log('mySource', mySource.sortBysAvailable);
-    //   })
-    //   //console.log('this.getSorts', this.getSorts());
-    //   console.log('this.state.articles', this.state.articles);
-
   }
 
 
@@ -41,21 +33,19 @@ export default class Articles extends React.Component {
     this.setState({
       articles: ArticlesStore.getAll()
     });
-    //console.log(this.state.articles,'aticles state');
   }
-  // getSorts(){
-  //   const mySources = this.state.sources;
-  //   mySources.map((mySource) => {
-  //     <option>{}</option>
-  //     console.log('mySource', mySource.sortBysAvailable);
-  //   })
-  //   //console.log('this.getSorts', this.getSorts());
-  //   console.log('this.state.articles', this.state.articles);
 
-  
+  handleChange(event){
+    console.log('events',event.target.value)
+    console.log()
+    this.setState(getArticles(this.props.params.article, event.target.value));  
+  }
+ 
+
   render() {
-    console.log("params of article", this.props.location.query)
-    let sorts = this.props.location.query
+    //console.log("params of article", this.props.location.query)
+    let sorts = this.props.location.query.sort
+    sorts = sorts.split(',');
     console.log('sorts', sorts)
     const { articles } = this.state;
     //console.log(this.state);
@@ -65,27 +55,16 @@ export default class Articles extends React.Component {
         <p>Articles from over 70 sources</p>
 
         <div>
-          {/*{sorts.map((sort,index) => {
-
-            return
-            <div>
-            <option onChange={handleChange}>{sort}</option>
-            </div>
-          })}*/}
-
-          {/*{sorts.map((sort, index) => {
-            //console.log(article);
-            return <div>
-              <h4><a href={article.url} target="_blank">{sort}</a></h4>
-            </div>
-          })}*/}
+            <select class="form-control" id="select" onChange={this.handleChange.bind(this)}>{sorts.map(function(type,index){
+                  return <option value = {type}>{type}</option>;
+              })}</select>
         </div>
+      
         <div>
-
           {articles.map((article, index) => {
             //console.log(article);
             return <div key={index}>
-              <h4><a href={article.url} target="_blank">{article.author}</a></h4>
+              <h4><a href={article.url} target="_blank">Author: {article.author}</a></h4>
               <p>{article.description}</p>
             </div>
           })}
