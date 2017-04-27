49c8002f481d6dea54a95b1afe16edc8a6a2d37a
Deborah Oni <deborah.oni@andela.com>
2017-04-27 17:21:58 +0100

    set up to prevent users from viewing sources without logging in

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -6,29 +6,31 @@ import Nav from "../components/layout/Nav";
 const isLoggedIn = Cookies.get('debprojdb');
 import createHistory from 'history/createBrowserHistory';
 import AuthenticationStore from '../stores/AuthenticationStore';
-import {isLoggin} from '../actions/AuthenticationAction';
+import { isLoggin } from '../actions/AuthenticationAction';
+import Login from './Login';
 const history = createHistory({
   forceRefresh: true
 })
 export default class Layout extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.login = this.login.bind(this);
     this.state = this.userState();
+    console.log(this.state);
   }
-  
+
   componentDidMount() {
     AuthenticationStore.addChangeListener(this.login);
     isLoggin();
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     AuthenticationStore.removeChangeListener(this.login)
   }
-  login(){
+  login() {
     this.setState(this.userState())
   }
 
-  userState(){
+  userState() {
     return {
       user: AuthenticationStore.getUser()
     }
@@ -36,29 +38,36 @@ export default class Layout extends React.Component {
 
 
   render() {
+    console.log('user logged in', this.state.user.isAuthenticated)
     const { location } = this.props;
     const containerStyle = {
       marginTop: "60px"
-    };
-    console.log(this.state.user);
-    return (
+    }; //console.log(this.state.user);
+
+    if (!this.state.user.isAuthenticated) {
+      return (<div>
+                <Login />
+               </div>)
+    }
+      return (
+
       <div>
 
-        <Nav location={location} isLoggedIn={this.state.user.isAuthenticated}  />
+            <Nav location={location} isLoggedIn={this.state.user.isAuthenticated} />
 
-        <div class="container" style={containerStyle}>
-          <div class="row">
-            <div class="col-lg-12">
-              <h1>Headlines</h1>
+            <div class="container" style={containerStyle}>
+              <div class="row">
+                <div class="col-lg-12">
+                  <h1>Headlines</h1>
 
-              {this.props.children}
+                  {this.props.children}
 
+                </div>
+              </div>
+              <Footer />
             </div>
           </div>
-          <Footer/>
-        </div>
-      </div>
 
-    );
+          );
   }
 }
