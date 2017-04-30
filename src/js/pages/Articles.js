ed934dc13e5df7e7e475619bb801b5c603558de0
Deborah Oni <deborah.oni@andela.com>
2017-04-30 13:52:50 +0100

    user interface design 1

--- src/js/pages/Articles.js
+++ src/js/pages/Articles.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Clearfix from 'react-clearfix';
+import Icon, { Telegram } from 'react-share-icons';
+import Instagram from 'react-share-icons/lib/Instagram';
 import ArticlesStore from '../stores/ArticlesStore';
 import { getArticles } from '../actions/ArticlesActions';
 import { getSources } from '../actions/SourcesActions';
@@ -11,7 +14,7 @@ export default class Articles extends React.Component {
     super();
     this.state = {
       articles: ArticlesStore.getAll(),
-      sortType:''
+      sortType: ''
     };
 
     this.getArticles = this.getArticles.bind(this);
@@ -35,12 +38,12 @@ export default class Articles extends React.Component {
     });
   }
 
-  handleChange(event){
-    console.log('events',event.target.value)
+  handleChange(event) {
+    console.log('events', event.target.value)
     console.log()
-    this.setState(getArticles(this.props.params.article, event.target.value));  
+    this.setState(getArticles(this.props.params.article, event.target.value));
   }
- 
+
 
   render() {
     //console.log("params of article", this.props.location.query)
@@ -50,23 +53,31 @@ export default class Articles extends React.Component {
     const { articles } = this.state;
     //console.log(this.state);
     return (
-      <div class="jumbotron">
-        <h1>Headlines articles</h1>
-        <p>Articles from over 70 sources</p>
-
+      <div >
         <div>
-            <select className="form-control" id="select" onChange={this.handleChange.bind(this)}>{sorts.map(function(type,index){
-                  return <option value = {type}>{type}</option>;
-              })}</select>
+          <select className="form-control" id="select" onChange={this.handleChange.bind(this)}>{sorts.map(function (type, index) {
+            return <option value={type}>{type}</option>;
+          })}</select>
         </div>
-      
-        <div>
+
+        <div className="card-group">
           {articles.map((article, index) => {
             //console.log(article);
-            return <div key={index}>
-              <h4><a href={article.url} target="_blank">Author: {article.author}</a></h4>
-              <p>{article.description}</p>
-            </div>
+            return (
+              <div class="row">
+                <div className="col-md-4 "><img class="card-img-top img-responsive " src={article.urlToImage} alt="Card image cap"></img></div>
+                <div className="panel panel-default  col-md-8" key={article.url}>
+                  <div className="panel-heading"><a href={article.url} target="_blank">Title: {article.title}</a> {article.publishedAt}</div>
+                  <div className="panel-body">
+                    <p> {article.description} </p>
+                     <a href="https://www.facebook.com/sharer/sharer.php?u={articles.url}" target="_blank"> <Icon type="Facebook" className="shares-facebook" width ="3em" height="3em" color="#333"/></a>
+                     <a href="http://twitter.com/share?text=Articles&url<?=urlencode($url)?>"target="_blank"title="Click to post to Twitter"> <Icon type="Twitter" className="shares-twitter" width ="3em" height="3em"/></a>
+                     <a href=""><Instagram className="shares-instagram" width ="3em" height="3em"/></a>
+                    {/*<Clearfix/>*/}
+                  </div>
+                </div>
+              </div>
+            )
           })}
         </div>
 
