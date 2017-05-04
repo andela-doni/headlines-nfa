cb0dff1c96e3058bc2de710dbee988a0a5bd5175
Deborah Oni <deborah.oni@andela.com>
2017-05-04 10:35:36 +0100

    remove clearfix use

--- src/js/pages/Articles.js
+++ src/js/pages/Articles.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import Clearfix from 'react-clearfix';
 import Icon, { Telegram } from 'react-share-icons';
 import Instagram from 'react-share-icons/lib/Instagram';
 import ArticlesStore from '../stores/ArticlesStore';
@@ -75,7 +74,7 @@ export default class Articles extends React.Component {
                      <a href="https://www.facebook.com/sharer/sharer.php?u={articles.url}" target="_blank"> <Icon type="Facebook" className="shares-facebook" width ="3em" height="3em" color="#333"/></a>
                      <a href="http://twitter.com/share?text=Articles&url<?=urlencode($url)?>"target="_blank"title="Click to post to Twitter"> <Icon type="Twitter" className="shares-twitter" width ="3em" height="3em"/></a>
                      <a href=""><Instagram className="shares-instagram" width ="3em" height="3em"/></a>
-                    {/*<Clearfix/>*/}
+                    
                   </div>
                 </div>
               </div>
