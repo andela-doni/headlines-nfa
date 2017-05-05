317249806dcc8b8c4d8867bea90d132631709f5c
Deborah Oni <deborah.oni@andela.com>
2017-05-05 11:47:25 +0100

    add article spec for articles.js

--- __tests__/ArticlesSpec.js
+++ __tests__/ArticlesSpec.js
@@ -5,7 +5,7 @@ import sinon from 'sinon';
 import { shallow, mount } from 'enzyme';
 import Articles from '../src/js/pages/Articles';
 
-xdescribe('Articles Component', () => {
+describe('Articles Component', () => {
     const wrapper = mount(<Articles />);
     it('it should render div elements', () => {
         expect(wrapper.find('div')).toExist
